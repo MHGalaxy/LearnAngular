1598d27234637282ee6b71ffc3e1b0dd6a985e7e
Mohammad Hosein Kahkeshan <mo.ho.kahkeshan@gmail.com>
2025-10-26 21:24:04 +0330

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ProductListComponent} from './Components/product-list/product-list.component';
 
+//decorator factory
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
