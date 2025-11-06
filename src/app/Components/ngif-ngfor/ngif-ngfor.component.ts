12eb5de0fce933b9bd2086326e9e54ebbdc0f98a
Mohammad Hosein Kahkeshan <mo.ho.kahkeshan@gmail.com>
2025-11-06 15:33:29 +0330

    add ngFor syntax

--- src/app/Components/ngif-ngfor/ngif-ngfor.component.ts
+++ src/app/Components/ngif-ngfor/ngif-ngfor.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NgIf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 
 @Component({
@@ -7,11 +7,28 @@ import {FormsModule} from '@angular/forms';
   standalone: true,
   imports: [
     NgIf,
-    FormsModule
+    FormsModule,
+    NgForOf
   ],
   templateUrl: './ngif-ngfor.component.html',
   styleUrl: './ngif-ngfor.component.css'
 })
 export class NgifNgforComponent {
   userIsActive:boolean = false;
+  products=[
+    {
+      id:1,
+      name:"product 1",
+      price:1000,
+    },
+    {
+      id:2,
+      name:"product 2",
+      price:2000,
+    },
+    {
+      id:3,
+      name:"product 3",
+      price:3000,
+    }]
 }
