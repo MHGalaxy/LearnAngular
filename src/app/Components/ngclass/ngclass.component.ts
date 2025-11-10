ad68caf15c19bee29ce27874d824ad7ac8ea9b44
MHGalaxy <mo.ho.kahkeshan@gmail.com>
2025-11-10 18:47:35 +0330

    add ngStyle attr

--- src/app/Components/ngclass/ngclass.component.ts
+++ src/app/Components/ngclass/ngclass.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import {NgClass} from '@angular/common';
+import {NgClass, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-ngclass',
   standalone: true,
   imports: [
-    NgClass
+    NgClass,
+    NgStyle
   ],
   templateUrl: './ngclass.component.html',
   styleUrl: './ngclass.component.css'
@@ -15,4 +16,16 @@ export class NgclassComponent {
   secondClass: string = "text-success text-center";
 
   isActiveForthClass: boolean = true;
+
+  fourthStyle = {
+    fontSize: '12px',
+    fontWeight: 'bold',
+    textAlign: 'right',
+    backgroundColor: 'gray',
+    //'backgroundColor': 'gray', //OK
+    //'background-color': 'gray' //OK
+    //background-color: 'gray' //NOT OK
+  }
+
+  isDanger = false;
 }
