45f3d76a6f607241d5a28b9f846b5c347de1ef1c
Mohammad Hosein Kahkeshan <mo.ho.kahkeshan@gmail.com>
2025-11-25 01:08:40 +0330

    add first directive

---
+++ src/app/Directives/para-highlighter.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ParaHighlighterDirective } from './para-highlighter.directive';
+
+describe('ParaHighlighterDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ParaHighlighterDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/Directives/para-highlighter.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appParaHighlighter]',
+  standalone: true
+})
+export class ParaHighlighterDirective {
+
+  constructor() { }
+
+}
